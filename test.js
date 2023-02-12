0e774526522ed61700be3104c286942852550e1b
Maksim <khitrov.maks@gmail.com>
2023-02-12 17:47:09 +0400

    Changed to map

---
+++ test.js
@@ -0,0 +1,10 @@
+const listOfCommands = [
+  {command: '/start', description: 'List of commands'},
+  {command: '/help', description: 'List of commands'},
+  {command: '/about', description: 'About myself'},
+  {command: '/links', description: 'List of my social networks'},
+];
+
+
+const getSerializedList = (listOfCommands) =>
+
